b630635aa1859bedadcc82b19359367269c3998c
BarikQ <yar.barik99@gmail.com>
2019-03-14 21:35:13 +0300

    finish the task

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-  var counter;
-  var trianglesCounter;
-  var N = preferences.length;
+  let counter = 0;
 
+  for(var i = 0; i < preferences.length; i++){
+    let condition_1 = preferences[preferences[preferences[i] - 1] - 1] === i + 1;
+    if(condition_1){
+      counter ++;
+    }
+  }
+    return (Math.floor(counter/3));
 };
