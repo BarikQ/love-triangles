db8cd2564cdef0543561f559c0d8d53350d5f37b
BarikQ <yar.barik99@gmail.com>
2019-03-14 21:37:39 +0300

    1

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
   let counter = 0;
 
   for(var i = 0; i < preferences.length; i++){
-    let condition_1 = preferences[preferences[preferences[i] - 1] - 1] === i + 1;
-    if(condition_1){
+    let condition = preferences[preferences[preferences[i] - 1] - 1] === i + 1;
+    if(condition){
       counter ++;
     }
   }
